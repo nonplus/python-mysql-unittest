7b80d418752459a2f5e02e49edec4030651a69be
Stepan Riha <github@nonplus.net>
2023-04-05 22:31:05 -0500

    Update test_setup.sql

--- tests/test_setup.sql
+++ tests/test_setup.sql
@@ -1,8 +1,5 @@
+-- Initialize Student table
 DROP TABLE IF EXISTS Student;
-
-DROP TABLE IF EXISTS Test;
-
--- Initialize table
 CREATE TABLE `Student` (
     `ID` int NOT NULL AUTO_INCREMENT,
     `Name` varchar(45) NOT NULL,
@@ -12,4 +9,4 @@ CREATE TABLE `Student` (
     PRIMARY KEY (`ID`)
     ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Note: Use ENGINE=MEMORY for better performance
+-- Note: Use ENGINE=MEMORY for better test performance
